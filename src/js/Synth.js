d1f77fbd6bf05e1ca6f28da04dd43d0fcf7f7b89
Lucas Dachman <lucas.dachman@gmail.com>
2019-04-03 05:07:38 -0600

    refactor synth file to class

--- src/js/Synth.js
+++ src/js/Synth.js
@@ -1,32 +1,35 @@
 import Tone from 'tone';
 
-const synth = new Tone.PolySynth(6, Tone.Synth).toMaster();
-let chords = [];
+export default class Synth {
+  synth = new Tone.PolySynth(6, Tone.Synth).toMaster();
+  chords = [];
+  step = 0;
 
-let step = 0;
-let loop = new Tone.Loop(function(time) {
-  console.log(chords[step]);
-  
-  synth.triggerAttackRelease(chords[step], '8n', time);
-  step++;
-  if (step >= chords.length) step = 0;
-}, '8n').start(0);
-loop.humanize = true;
+  loop = new Tone.Loop(function (time) {
+    this.synth.triggerAttackRelease(this.chords[this.step], '8n', time);
+    this.step++;
+    if (this.step >= this.chords.length) this.step = 0;
+  }, '8n').start(0);
 
-export function playNote(note) {
-  Tone.context.resume();
-  synth.triggerAttackRelease(note, "4n");
-}
+  constructor() {
+    this.loop.humanize = true;
+  }
 
-export function setNotes(notes) {
-  chords = notes;
-}
+  playNote(note) {
+    Tone.context.resume();
+    this.synth.triggerAttackRelease(note, "4n");
+  }
 
-export function start() {
-  Tone.context.resume();
-  Tone.Transport.start();
-}
+  setNotes(chords) {
+    this.chords = chords;
+  }
 
-export function stop() {
-  Tone.Transport.stop();
+  start() {
+    Tone.context.resume();
+    Tone.Transport.start();
+  }
+
+  stop() {
+    Tone.Transport.stop();
+  }
 }
