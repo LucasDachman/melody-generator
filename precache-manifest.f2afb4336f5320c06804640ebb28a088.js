d1f06903b8a1184a3f60340a2dc3cb24b572dbda
Lucas Dachman <lucas.dachman@gmail.com>
2019-04-04 21:59:21 -0600

    Updates

---
+++ precache-manifest.f2afb4336f5320c06804640ebb28a088.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1f59114f1e7099abac1",
+    "url": "/melody-generator/static/css/main.5bea0a62.chunk.css"
+  },
+  {
+    "revision": "f1f59114f1e7099abac1",
+    "url": "/melody-generator/static/js/main.9cee3751.chunk.js"
+  },
+  {
+    "revision": "63736a97a0cd43e1dc95",
+    "url": "/melody-generator/static/js/runtime~main.82a8b910.js"
+  },
+  {
+    "revision": "73f8f83f72993e3e5a55",
+    "url": "/melody-generator/static/css/2.446d365f.chunk.css"
+  },
+  {
+    "revision": "73f8f83f72993e3e5a55",
+    "url": "/melody-generator/static/js/2.a3084282.chunk.js"
+  },
+  {
+    "revision": "36bfd548f0c7fe1ad5ff07724b0470a0",
+    "url": "/melody-generator/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.98fc6ae07a2a2dd72667382b33db9d97.js"
+  "/melody-generator/precache-manifest.f2afb4336f5320c06804640ebb28a088.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/melody-generator/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
